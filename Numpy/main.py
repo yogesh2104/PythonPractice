a2a7d07045c7a0debb4330d7b8f413a2cc8f440b
iamyogesh21 <singhyogesh2104>
2021-06-27 09:30:32 +0530

    Update main.py

--- Numpy/main.py
+++ Numpy/main.py
@@ -1,50 +1,50 @@
 import numpy as np
-# data = [8,89,67,55,46,3,22,56,78,12,32,11,5,76,90]
-# data2 = [82,80,61,53,47,39,22,52,78,12,30,16,5,78,90]
+data = [8,89,67,55,46,3,22,56,78,12,32,11,5,76,90]
+data2 = [82,80,61,53,47,39,22,52,78,12,30,16,5,78,90]
 
 
 
-# # sort() function return a sorted copy of an array.
-# print(np.sort(data))
-# print()
+# sort() function return a sorted copy of an array.
+print(np.sort(data))
+print()
 
-# # output   [ 3  5  8 11 12 22 32 46 55 56 67 76 78 89 90]
-
-
-
-# """greater_equal()it compare two list and return true or False.if the number is greater or 
-#  equal return ##! True 
-#  else retuen ##! False"""
-# print(np.greater_equal(data,data2))
-# print()
-
-# # output [False  True  True  True False False  True  True  True  True  True False True False  True]
+# output   [ 3  5  8 11 12 22 32 46 55 56 67 76 78 89 90]
 
 
 
+"""greater_equal()it compare two list and return true or False.if the number is greater or 
+ equal return ##! True 
+ else retuen ##! False"""
+print(np.greater_equal(data,data2))
+print()
 
-# """greater() it compare two list and return true or False.if the number is greater then return ##!True 
-#  else retuen ##! False"""
-# print(np.greater(data, data2))
-# print()
-
-# # output [False  True  True  True False False False  True False False  True False False False False]
-
-# """less_equal() it compare two list and return true or False.if the number is less or equal then
-# return ##! True 
-# else retuen ##! False"""
-# print(np.less_equal(data, data2))
-# print()
-
-# # output [ True False False False  True  True  True False  True  True False  True True  True  True]
+# output [False  True  True  True False False  True  True  True  True  True False True False  True]
 
 
 
-# """less() it compare two list and return true or False.if the number is less then return ##!True 
-# else retuen ##! False"""
-# print(np.less(data,data2))
 
-# # output [ True False False False  True  True False False False False False  True False  True False]
+"""greater() it compare two list and return true or False.if the number is greater then return ##!True 
+ else retuen ##! False"""
+print(np.greater(data, data2))
+print()
+
+# output [False  True  True  True False False False  True False False  True False False False False]
+
+"""less_equal() it compare two list and return true or False.if the number is less or equal then
+return ##! True 
+else retuen ##! False"""
+print(np.less_equal(data, data2))
+print()
+
+# output [ True False False False  True  True  True False  True  True False  True True  True  True]
+
+
+
+"""less() it compare two list and return true or False.if the number is less then return ##!True 
+else retuen ##! False"""
+print(np.less(data,data2))
+
+# output [ True False False False  True  True False False False False False  True False  True False]
 
 
 ##? broadcasting in numpy
@@ -75,3 +75,5 @@ the data array containing only those elements for which the
 indexing array contains “True” Boolean values at the respective
 array positions. Thus, the resulting array contains the three
 values 3, 7, and 8 """
+
+
