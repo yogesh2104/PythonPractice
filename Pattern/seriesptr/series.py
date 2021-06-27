437c466a63d643ccc6ec11151ce3ba4920ce3ffd
iamyogesh21 <singhyogesh2104>
2021-06-27 09:27:15 +0530

    add my practice.

---
+++ Pattern/seriesptr/series.py
@@ -0,0 +1,34 @@
+# Arithmetic series
+"""[Print]
+    1, 4, 7, 10, 13, 16,....
+
+"""
+#number of limit to print
+n = 5  
+#difference between two number
+d = 3 
+ #terms
+a = 1 
+
+for x in range(1,n+1):
+    print("" + str(a),end=",")
+    a =a+d
+
+
+
+
+# Geometric Series
+    """[print]
+    5,10,20,40,80,160,....
+    """
+#number of limit to print
+n = 7 
+#ratio diff between two number
+r = 2 
+#terms
+a = 5 
+for x in range(1,n+1):
+    print(""+str(a),end=",")
+    a =a*r
+
+
