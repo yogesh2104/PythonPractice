add1c4d1251a2bc2ce836f2089ca2c7cb097939b
yogesh2104 <singhyogesh2104@gmail.com>
2023-12-01 01:13:40 +0530

    python scrap

---
+++ bookScrap/bookscrapedata/bookscrapedata/spiders/books.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+    allowed_domains = ["toscrape.com"]
+    start_urls = ["https://toscrape.com"]
+
+    def parse(self, response):
+        pass
